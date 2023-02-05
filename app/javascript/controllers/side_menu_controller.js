b1d1e551d5ace55d83a3d8eb330b6768a3905104
Fluptu <fluptu@gmail.com>
2023-02-05 14:47:37 +0100

    layout and locale url fix

--- app/javascript/controllers/side_menu_controller.js
+++ app/javascript/controllers/side_menu_controller.js
@@ -2,21 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   sideMenuToggle() {
-    if(document.getElementById("navBar").style.width == "20vh")
+    if(document.getElementById("navBar").style.width === "20vh")
     {
-      document.getElementById("navBar").style.width = "8vh";
+      document.getElementById("navBar").style.width = "7vh";
       document.getElementById("homePageText").style.display = 'none';
       document.getElementById("homeOSPageText").style.display = 'none';
     }
     else
     {
       document.getElementById("navBar").style.width = "20vh";
-      document.getElementById("homePageText").style.fontSize = "16px";
-      document.getElementById("homePageText").style.padding = "0px 0px 0px 5px";
-      document.getElementById("homePageText").style.display = 'inline';
-      document.getElementById("homeOSPageText").style.fontSize = "16px";
-      document.getElementById("homeOSPageText").style.padding = "0px 0px 0px 5px";
-      document.getElementById("homeOSPageText").style.display = 'inline';
+      setTimeout(function (){
+        document.getElementById("homePageText").style.fontSize = "16px";
+        document.getElementById("homePageText").style.padding = "0px 0px 0px 5px";
+        document.getElementById("homePageText").style.display = 'inline';
+        document.getElementById("homeOSPageText").style.fontSize = "16px";
+        document.getElementById("homeOSPageText").style.padding = "0px 0px 0px 5px";
+        document.getElementById("homeOSPageText").style.display = 'inline';
+      },200)
     }
   }
 }
