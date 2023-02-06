7faf9c58f56cf8aba1257c917464d255f5971564
Fluptu <fluptu@gmail.com>
2023-02-06 20:36:51 +0100

    layout and locale url fix

--- app/javascript/controllers/side_menu_controller.js
+++ app/javascript/controllers/side_menu_controller.js
@@ -21,4 +21,23 @@ export default class extends Controller {
       },200)
     }
   }
+
+  sideMenuToggleMedia(){
+    if(document.getElementById("navBar").style.width === "40vh")
+    {
+      document.getElementById("navBar").style.width = '0';
+    }
+    else
+    {
+      document.getElementById("navBar").style.width = "40vh";
+      setTimeout(function (){
+        document.getElementById("homePageText").style.fontSize = "16px";
+        document.getElementById("homePageText").style.padding = "0px 0px 0px 5px";
+        document.getElementById("homePageText").style.display = 'inline';
+        document.getElementById("homeOSPageText").style.fontSize = "16px";
+        document.getElementById("homeOSPageText").style.padding = "0px 0px 0px 5px";
+        document.getElementById("homeOSPageText").style.display = 'inline';
+      },200)
+    }
+  }
 }
