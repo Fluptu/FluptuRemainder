a468dfec42898af0950e8b15c8323e07c9fbb982
Fluptu <fluptu@gmail.com>
2023-02-15 20:51:51 +0100

    devise parameter sanitization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    #before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     add_flash_types :info, :success , :error
     around_action :switch_locale
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
         { locale: I18n.locale }
     end
 
-    #protected
+    protected
 
-    #def configure_permitted_parameters
-    #    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-    #    devise_parameter_sanitizer.permit(:sign_in, keys: %i[ email username])
-    #end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[ email username])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [ :username ])
+    end
 end
